367a2b5beba02ef935dd8b845e34ae79c9541446
genfengl <genfeng.liu@gmail.com>
2023-02-01 15:10:58 +0800

    added start button and a 3 seconds countdown in the game component. Still need to work on a better implementation of the game functionality itself

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,358 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 
 const Game = () => {
-  return (
-    <div>Game</div>
-  )
+    const [gameStart, setGameStart] = useState(false)
+    const [timeLeft, setTimeLeft] = useState(3)
+    const [flagCountryCode, setFlagCountryCode] = useState('za')
+    const [attemptedQuestion, setAttemptedQuestion] = useState(0)
+    const [correctAnswerChoice, setCorrectAnswerChoice] = useState()
+    const [correctAnswer, setCorrectAnswer] = useState()
+    const [answer0, setAnswer0] = useState()
+    const [answer1, setAnswer1] = useState()
+    const [answer2, setAnswer2] = useState()
+    const [answer3, setAnswer3] = useState()
+
+
+    // Country Flag list with code for api requests
+    // 249 key-value pairs in the object
+    const countryList = {
+        "AF": "Afghanistan",
+        "AL": "Albania",
+        "DZ": "Algeria",
+        "AS": "American Samoa",
+        "AD": "Andorra",
+        "AO": "Angola",
+        "AI": "Anguilla",
+        "AQ": "Antarctica",
+        "AG": "Antigua and Barbuda",
+        "AR": "Argentina",
+        "AM": "Armenia",
+        "AW": "Aruba",
+        "AU": "Australia",
+        "AT": "Austria",
+        "AZ": "Azerbaijan",
+        "BS": "Bahamas",
+        "BH": "Bahrain",
+        "BD": "Bangladesh",
+        "BB": "Barbados",
+        "BY": "Belarus",
+        "BE": "Belgium",
+        "BZ": "Belize",
+        "BJ": "Benin",
+        "BM": "Bermuda",
+        "BT": "Bhutan",
+        "BO": "Bolivia (Plurinational State of)",
+        "BQ": "Bonaire, Sint Eustatius and Saba",
+        "BA": "Bosnia and Herzegovina",
+        "BW": "Botswana",
+        "BV": "Bouvet Island",
+        "BR": "Brazil",
+        "IO": "British Indian Ocean Territory",
+        "BN": "Brunei Darussalam",
+        "BG": "Bulgaria",
+        "BF": "Burkina Faso",
+        "BI": "Burundi",
+        "CV": "Cabo Verde",
+        "KH": "Cambodia",
+        "CM": "Cameroon",
+        "CA": "Canada",
+        "KY": "Cayman Islands",
+        "CF": "Central African Republic",
+        "TD": "Chad",
+        "CL": "Chile",
+        "CN": "China",
+        "CX": "Christmas Island",
+        "CC": "Cocos (Keeling) Islands",
+        "CO": "Colombia",
+        "KM": "Comoros",
+        "CD": "Congo (the Democratic Republic of the)",
+        "CG": "Congo",
+        "CK": "Cook Islands",
+        "CR": "Costa Rica",
+        "HR": "Croatia",
+        "CU": "Cuba",
+        "CW": "Curaçao",
+        "CY": "Cyprus",
+        "CZ": "Czechia",
+        "CI": "Côte d'Ivoire",
+        "DK": "Denmark",
+        "DJ": "Djibouti",
+        "DM": "Dominica",
+        "DO": "Dominican Republic",
+        "EC": "Ecuador",
+        "EG": "Egypt",
+        "SV": "El Salvador",
+        "GQ": "Equatorial Guinea",
+        "ER": "Eritrea",
+        "EE": "Estonia",
+        "SZ": "Eswatini",
+        "ET": "Ethiopia",
+        "FK": "Falkland Islands [Malvinas]",
+        "FO": "Faroe Islands",
+        "FJ": "Fiji",
+        "FI": "Finland",
+        "FR": "France",
+        "GF": "French Guiana",
+        "PF": "French Polynesia",
+        "TF": "French Southern Territories",
+        "GA": "Gabon",
+        "GM": "Gambia",
+        "GE": "Georgia",
+        "DE": "Germany",
+        "GH": "Ghana",
+        "GI": "Gibraltar",
+        "GR": "Greece",
+        "GL": "Greenland",
+        "GD": "Grenada",
+        "GP": "Guadeloupe",
+        "GU": "Guam",
+        "GT": "Guatemala",
+        "GG": "Guernsey",
+        "GN": "Guinea",
+        "GW": "Guinea-Bissau",
+        "GY": "Guyana",
+        "HT": "Haiti",
+        "HM": "Heard Island and McDonald Islands",
+        "VA": "Holy See",
+        "HN": "Honduras",
+        "HK": "Hong Kong",
+        "HU": "Hungary",
+        "IS": "Iceland",
+        "IN": "India",
+        "ID": "Indonesia",
+        "IR": "Iran (Islamic Republic of)",
+        "IQ": "Iraq",
+        "IE": "Ireland",
+        "IM": "Isle of Man",
+        "IL": "Israel",
+        "IT": "Italy",
+        "JM": "Jamaica",
+        "JP": "Japan",
+        "JE": "Jersey",
+        "JO": "Jordan",
+        "KZ": "Kazakhstan",
+        "KE": "Kenya",
+        "KI": "Kiribati",
+        "KP": "Korea (the Democratic People's Republic of)",
+        "KR": "Korea (the Republic of)",
+        "KW": "Kuwait",
+        "KG": "Kyrgyzstan",
+        "LA": "Lao People's Democratic Republic",
+        "LV": "Latvia",
+        "LB": "Lebanon",
+        "LS": "Lesotho",
+        "LR": "Liberia",
+        "LY": "Libya",
+        "LI": "Liechtenstein",
+        "LT": "Lithuania",
+        "LU": "Luxembourg",
+        "MO": "Macao",
+        "MG": "Madagascar",
+        "MW": "Malawi",
+        "MY": "Malaysia",
+        "MV": "Maldives",
+        "ML": "Mali",
+        "MT": "Malta",
+        "MH": "Marshall Islands",
+        "MQ": "Martinique",
+        "MR": "Mauritania",
+        "MU": "Mauritius",
+        "YT": "Mayotte",
+        "MX": "Mexico",
+        "FM": "Micronesia (Federated States of)",
+        "MD": "Moldova (the Republic of)",
+        "MC": "Monaco",
+        "MN": "Mongolia",
+        "ME": "Montenegro",
+        "MS": "Montserrat",
+        "MA": "Morocco",
+        "MZ": "Mozambique",
+        "MM": "Myanmar",
+        "NA": "Namibia",
+        "NR": "Nauru",
+        "NP": "Nepal",
+        "NL": "Netherlands",
+        "NC": "New Caledonia",
+        "NZ": "New Zealand",
+        "NI": "Nicaragua",
+        "NE": "Niger",
+        "NG": "Nigeria",
+        "NU": "Niue",
+        "NF": "Norfolk Island",
+        "MP": "Northern Mariana Islands",
+        "NO": "Norway",
+        "OM": "Oman",
+        "PK": "Pakistan",
+        "PW": "Palau",
+        "PS": "Palestine, State of",
+        "PA": "Panama",
+        "PG": "Papua New Guinea",
+        "PY": "Paraguay",
+        "PE": "Peru",
+        "PH": "Philippines",
+        "PN": "Pitcairn",
+        "PL": "Poland",
+        "PT": "Portugal",
+        "PR": "Puerto Rico",
+        "QA": "Qatar",
+        "MK": "Republic of North Macedonia",
+        "RO": "Romania",
+        "RU": "Russian Federation",
+        "RW": "Rwanda",
+        "RE": "Réunion",
+        "BL": "Saint Barthélemy",
+        "SH": "Saint Helena, Ascension and Tristan da Cunha",
+        "KN": "Saint Kitts and Nevis",
+        "LC": "Saint Lucia",
+        "MF": "Saint Martin (French part)",
+        "PM": "Saint Pierre and Miquelon",
+        "VC": "Saint Vincent and the Grenadines",
+        "WS": "Samoa",
+        "SM": "San Marino",
+        "ST": "Sao Tome and Principe",
+        "SA": "Saudi Arabia",
+        "SN": "Senegal",
+        "RS": "Serbia",
+        "SC": "Seychelles",
+        "SL": "Sierra Leone",
+        "SG": "Singapore",
+        "SX": "Sint Maarten (Dutch part)",
+        "SK": "Slovakia",
+        "SI": "Slovenia",
+        "SB": "Solomon Islands",
+        "SO": "Somalia",
+        "ZA": "South Africa",
+        "GS": "South Georgia and the South Sandwich Islands",
+        "SS": "South Sudan",
+        "ES": "Spain",
+        "LK": "Sri Lanka",
+        "SD": "Sudan",
+        "SR": "Suriname",
+        "SJ": "Svalbard and Jan Mayen",
+        "SE": "Sweden",
+        "CH": "Switzerland",
+        "SY": "Syrian Arab Republic",
+        "TW": "Taiwan",
+        "TJ": "Tajikistan",
+        "TZ": "Tanzania, United Republic of",
+        "TH": "Thailand",
+        "TL": "Timor-Leste",
+        "TG": "Togo",
+        "TK": "Tokelau",
+        "TO": "Tonga",
+        "TT": "Trinidad and Tobago",
+        "TN": "Tunisia",
+        "TR": "Turkey",
+        "TM": "Turkmenistan",
+        "TC": "Turks and Caicos Islands",
+        "TV": "Tuvalu",
+        "UG": "Uganda",
+        "UA": "Ukraine",
+        "AE": "United Arab Emirates",
+        "GB": "United Kingdom of Great Britain and Northern Ireland",
+        "UM": "United States Minor Outlying Islands",
+        "US": "United States of America",
+        "UY": "Uruguay",
+        "UZ": "Uzbekistan",
+        "VU": "Vanuatu",
+        "VE": "Venezuela (Bolivarian Republic of)",
+        "VN": "Viet Nam",
+        "VG": "Virgin Islands (British)",
+        "VI": "Virgin Islands (U.S.)",
+        "WF": "Wallis and Futuna",
+        "EH": "Western Sahara",
+        "YE": "Yemen",
+        "ZM": "Zambia",
+        "ZW": "Zimbabwe",
+        "AX": "Åland Islands"
+    };
+    const countryCode = Object.keys(countryList)
+
+    const handleStartButtonClick = () => {
+        setGameStart(true)
+    }
+
+    const gameStartTimer = setInterval(() => {
+        if (gameStart === true && timeLeft > 0) {
+            setTimeLeft(timeLeft - 1)
+            clearInterval(gameStartTimer)
+        }
+    }, 1000)
+
+    useEffect(() => {
+        const getRandomFlagCode = () => {
+            let random = Math.floor(Math.random() * countryCode.length)
+            let randomCode = countryCode[random]
+            console.log(randomCode)
+            setFlagCountryCode(randomCode.toLowerCase())
+            setCorrectAnswer(countryList[randomCode])
+        }
+        const getRandomCorrectAnswer = () => {
+            let random = Math.floor(Math.random() * 4)
+            setCorrectAnswerChoice(random)
+        }
+        const assignCorrectAnswer = () => {
+            if (correctAnswerChoice === 0) {
+                setAnswer0(correctAnswer)
+            } else if (correctAnswerChoice === 1) {
+                setAnswer1(correctAnswer)
+            } else if (correctAnswerChoice === 2) {
+                setAnswer2(correctAnswer)
+            } else if (correctAnswerChoice === 3) {
+                setAnswer3(correctAnswer)
+            }
+        }
+
+        getRandomFlagCode()
+        getRandomCorrectAnswer()
+        assignCorrectAnswer()
+        return () => {
+
+        }
+    }, [attemptedQuestion])
+    console.log(correctAnswer)
+    console.log(correctAnswerChoice)
+
+    return (
+        <div className='flex justify-center items-center h-full'>
+            <button onClick={handleStartButtonClick}
+                className={`p-3 m-6 text-2xl font-bold border rounded-xl ${gameStart ? 'hidden' : ''}
+            hover:bg-slate-700 hover:scale-105`}>
+                Start
+            </button>
+            <div className={`text-5xl ${gameStart && timeLeft > 0 ? '' : 'hidden'} 
+            animate-ping animation-delay-100`}>
+                {timeLeft}
+            </div>
+            {/* The game itself */}
+            <div>
+                {/* Flag */}
+                <div>
+                    <img
+                        src={`https://flagcdn.com/${flagCountryCode}.svg`}
+                        width="360"
+                        className={`${gameStart === true && timeLeft === 0 ? '' : 'hidden'}`}
+                    />
+                </div>
+                {/* Answers */}
+                <div className='grid'>
+                    <button>
+                        {answer0}
+                    </button>
+                    <button>
+                        {answer1}
+                    </button>
+                    <button>
+                        {answer2}
+                    </button>
+                    <button>
+                        {answer3}
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Game
