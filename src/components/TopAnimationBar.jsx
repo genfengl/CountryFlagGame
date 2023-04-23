8030e8fecf3da930c346aeac4eee6166fcfd6a9a
genfengl <genfeng.liu@gmail.com>
2023-04-23 21:39:01 +1000

    clean up

--- src/components/TopAnimationBar.jsx
+++ src/components/TopAnimationBar.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 // Generate the an array of flag img components for top animation bar
 const TopAnimationBar = ({sixtyFlagCodes}) => {
-    
     const flags = []
     // the component needs to have a copy of the 30 flags
     for (let i = 0; i < 2; i++) {
